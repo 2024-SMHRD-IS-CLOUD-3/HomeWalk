61b01c0cdfe8154e2f3b9325be0a9d5cfd598bb1
smhrd <smhrd@192.168.219.64>
2024-08-05 11:06:35 +0900

    최초 커밋

---
+++ src/test/java/com/example/homewalk/HomewalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homewalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomewalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
